827be8b85d9c66d7a8bf58a95aa0f16952427c98
ambydau <amberedaniels09@gmail.com>
2020-10-20 20:20:50 -0700

    updated readme

--- v2/script.js
+++ v2/script.js
@@ -1,39 +1,20 @@
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
-const playerScoreSpan = document.querySelector("#user");
-const computerScoreSpan = document.querySelector("#computer");
-const winnerSpan = document.querySelector("#winner");
 
-let userScore = 0;
-let computerScore = 0;
-
-function play(e) {
-    const computerChoice = "rock";
-    if(this.id === "rock") {
-        console.log("Tie!");
-    } else if (this.id === "paper") {
-        console.log("User Wins!");
-        userScore++;
-        playerScoreSpan.innerHTML = userScore;
-    } else {
-        console.log("Computer Wins!");
-        computerScore++;
-        computerScoreSpan.innerHTML = computerScore;
-    }
-    if(userScore === 5 || computerScore === 5) {
-        
-        reset();
-    } 
+function getComputerChoice() {
+    let computerChoice = "rock";
+    return computerChoice;
 }
 
-function reset() {
-    userScore = 0;
-    computerScore = 0;
-    playerScoreSpan.innerHTML = userScore;
-    computerScoreSpan.innerHTML = computerScore;
+
+function game() {
+    let userChoice = this.id;
+    let computerChoice = getComputerChoice();
+    console.log(computerChoice + userChoice);
 }
 
-rock.addEventListener("click", play);
-paper.addEventListener("click", play);
-scissors.addEventListener("click", play);
+
+rock.addEventListener("click", game);
+paper.addEventListener("click", game);
+scissors.addEventListener("click", game);
